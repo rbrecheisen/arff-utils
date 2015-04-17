5475ab466cd7013b7dbd548c232a72856c18b212
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2015-04-17 11:24:48 +0200

    Added test data and updated test case

--- tests/test_arff_utils.py
+++ tests/test_arff_utils.py
@@ -12,32 +12,45 @@ import os
 import unittest
 from arff_utils import ARFF
 
-IN_FILE = 'data/data.arff'
-OUT_FILE = 'data/data_out.arff'
+DIR = os.path.abspath('./tests')
+IN_FILE = DIR + '/data/data.arff'
+OUT_FILE = DIR + '/data/data_out.arff'
 
 
 class TestArffUtils(unittest.TestCase):
 
     def setUp(self):
-        pass
+
+    	# Get file names test data sets
+        self._data_dir = os.path.abspath('./tests/data')
+
+        self._iris  = self._data_dir + '/iris.arff'
+        self._labor = self._data_dir + '/labor.arff'
+        self._temp  = self._data_dir + '/temp.arff'
 
     def testIO(self):
         
-        # Read test data, write it to new file and read it back in
-        data = ARFF.read(IN_FILE)
-        ARFF.write(OUT_FILE, data)
-        data = ARFF.read(OUT_FILE)
+        # Read iris data
+        data = ARFF.read(self._iris)
+        ARFF.write(self._temp, data)
+        data = ARFF.read(self._temp)
 
-    def testToDataFrame():
+        # Read labor data
+        data = ARFF.read(self._labor)
+        ARFF.write(self._temp, data)
+        data = ARFF.read(self._temp)
 
-    	data = ARFF.read(IN_FILE)
+    def testToDataFrame(self):
+
+    	# Convert ARFF data to Pandas data frame
+    	data = ARFF.read(self._iris)
     	data_frame = ARFF.to_data_frame(data)
 
     def tearDown(self):
         
         # Clean up intermediate files
-        if os.path.isfile(OUT_FILE):
-        	os.remove(OUT_FILE)
+        if os.path.isfile(self._temp):
+        	os.remove(self._temp)
 
 
 if __name__ == '__main__':
